d2709cc173ddc3a856b5d0b047d616629490876e
etoal83 <etoal83@gmail.com>
2021-05-03 18:32:33 +0900

    Add hello_dice with css 3D

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 use seed::{prelude::*, *};
 use seed_styles::*;
+use seed_styles::{px, pc, rem};
 // use std::fmt;
 // use serde::{Serialize, Deserialize};
 
@@ -113,6 +114,7 @@ fn view(model: &Model) -> impl IntoNodes<Msg> {
             ],
             Page::NotFound => div!["404"],
         },
+        hello_dice(),
     ]
 }
 
@@ -130,6 +132,65 @@ fn header(base_url: &Url) -> Node<Msg> {
                 "About",
             ]],
         ],
+    ]
+}
+
+fn hello_dice() -> Node<Msg> {
+    div![C!["dice-container"],
+        s().position_absolute()
+            .top("50%")
+            .left("50%")
+            .perspective("500px")
+        ,
+        div![C!["dice"],
+            s().transform_style("preserve-3d")
+                .animation("10000ms linear infinite")
+                .keyframe(0, s().transform("rotateX(0deg) rotateY(0deg)"))
+                .keyframe(100, s().transform("rotateX(720deg) rotateY(360deg)"))
+            ,
+            ol![C!["dice-list"],
+                s().transform_style("preserve-3d")
+                    .transform("translateY(-85px) translateX(-85px)")
+                ,
+                dice_surface(1),
+                dice_surface(2),
+                dice_surface(3),
+                dice_surface(4),
+                dice_surface(5),
+                dice_surface(6),
+            ],
+        ],
+    ]
+}
+
+fn dice_surface(id: i32) -> Node<Msg> {
+    let base_style = s()
+        .position_absolute()
+        .width(px(150))
+        .height(px(150))
+        .background_color(CssColor::Rgba(150., 100., 255., 0.6))
+        .display(CssDisplay::Flex)
+        .justify_content(CssJustifyContent::Center)
+        .align_items(CssAlignItems::Center)
+        .font_size(rem(6))
+        .color(CssColor::Rgba(255., 255., 255., 0.6))
+        .text_shadow("0px 0px 10px rgba(255, 255, 255, 0.4)")
+        .border_radius(CssBorderRadius::Length(px(10)))
+        .box_shadow("0px 0px 15px rgba(200, 150, 255, 0.4)")
+        ;
+    let (s_char, s_style) = match id {
+        1 => ("H", base_style.clone().transform("translateZ(85px)")),
+        2 => ("E", base_style.clone().transform("translateY(85px) rotateX(270deg)")),
+        3 => ("L", base_style.clone().transform("translateX(85px) rotateX(180deg) rotateY(90deg)")),
+        4 => ("💚", base_style.clone().transform("translateX(-85px) rotateX(180deg) rotateY(-90deg)")),
+        5 => ("L", base_style.clone().transform("translateY(-85px) rotateY(180deg) rotateX(90deg)")),
+        6 => ("O", base_style.clone().font_size(rem(5)).transform("translateZ(-85px) rotateY(180deg)")),
+        _ => ("_", base_style),
+    };
+    li![
+        attrs! { At::from("surface-id") => id },
+        s_style,
+        s_char,
     ]
 }
 
